0b618d2b6c76a7f7bd984459f0e6d431d97d2f93
Vincenzo Melillo <137229406+Vince-mel@users.noreply.github.com>
2024-01-12 12:12:02 +0100

    Initial commit

---
+++ src/main/java/VincenzoMelillo/S5L5/S5L5Application.java
@@ -0,0 +1,13 @@
+package VincenzoMelillo.S5L5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S5L5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S5L5Application.class, args);
+	}
+
+}
---
+++ src/test/java/VincenzoMelillo/S5L5/S5L5ApplicationTests.java
@@ -0,0 +1,13 @@
+package VincenzoMelillo.S5L5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S5L5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
