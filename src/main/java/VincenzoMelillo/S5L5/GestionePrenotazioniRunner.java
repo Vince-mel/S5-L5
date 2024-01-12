aa2d70a1bd8232fa0d5f91f0b3f48d6c10a05aaa
Vincenzo Melillo <137229406+Vince-mel@users.noreply.github.com>
2024-01-12 16:03:06 +0100

    Update GestionePrenotazioniRunner.java

--- src/main/java/VincenzoMelillo/S5L5/GestionePrenotazioniRunner.java
+++ src/main/java/VincenzoMelillo/S5L5/GestionePrenotazioniRunner.java
@@ -2,7 +2,12 @@ package VincenzoMelillo.S5L5;
 
 
 import VincenzoMelillo.S5L5.entities.Edificio;
+import VincenzoMelillo.S5L5.entities.Postazione;
+import VincenzoMelillo.S5L5.entities.Tipo;
+import VincenzoMelillo.S5L5.entities.Utente;
 import VincenzoMelillo.S5L5.services.EdificioService;
+import VincenzoMelillo.S5L5.services.PostazioneService;
+import VincenzoMelillo.S5L5.services.UtenteService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,11 +23,17 @@ public class GestionePrenotazioniRunner implements ApplicationRunner {
 
     @Autowired
     EdificioService edificioSrv;
+    @Autowired
+    PostazioneService postazioneSrv;
+    @Autowired
+    UtenteService utenteSrv;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
 
         creaEdificio();
+        creaPostazione();
+        creaUtente();
     }
 
 
@@ -34,7 +45,30 @@ public class GestionePrenotazioniRunner implements ApplicationRunner {
 
         System.out.println(e);
         edificioSrv.saveEdificio(e);
-        log.info("Edificio creato in DB");
+        log.info("Edificio salvato nel DB");
+    }
+
+
+    public void creaPostazione() {
+        Postazione p = new Postazione();
+        p.setTipo(Tipo.SALA_RIUNIONI);
+        p.setDescrizione("Aula Magna");
+        p.setNumero_max_occupanti(30);
+
+        System.out.println(p);
+        postazioneSrv.savePostazione(p);
+        log.info("Postazione salvata nel DB");
+    }
+
+    public void creaUtente() {
+        Utente u = new Utente();
+        u.setNome_completo("Guido LaVespa");
+        u.setUsername("guido88");
+        u.setEmail("guido88@gmail.com");
+
+        System.out.println(u);
+        utenteSrv.saveUtente(u);
+        log.info("Utente salvato nel DB");
     }
 
 
